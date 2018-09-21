1c4828dbd1aeb880530d0daf135ac8c68d436715
Elgolfin <elgo@nicomputer.net>
2018-09-21 17:05:22 -0400

    Set the Offline.options object during the app creation

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
+/* global Offline */
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 require('@/offline.0.7.14.min.js')
+
+Offline.options = {
+  checkOnLoad: false,
+  interceptRequests: false,
+  requests: false,
+  game: false
+}
 
 Vue.config.productionTip = false
 
